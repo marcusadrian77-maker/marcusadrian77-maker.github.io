474c465c56c068d2ad425ade2bc34cb7ff5c8db1
marcusadrian77-maker <marcusadrian77@gmail.com>
2026-08-19 16:06:21 +0300

    Add 'mentenanta-periodica' to sitemap static paths

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -4,7 +4,7 @@ import { SECTOARE, BRANDURI } from '../data/sectoare';
 const statice = ['', 'despre-noi/', 'servicii/', 'preturi/', 'contact/', 'intrebari-frecvente-faq-2/',
  'reparatii-televizoare/', 'reparatii-laptopuri-bucuresti/', 'reparatii-macbook/', 'reparatii-boxe-active/',
  'reparatii-monitoare-lcd/', 'reparatii-cuptoare-cu-microunde/', 'reparatii-mentenanta-console-ps4-ps5-xbox-manete/',
- 'reparatii-controllere-ps4-ps5-xbox/', 'mentenanta-console-ps4-ps5-xbox/', 'mentenanta-profesionala-pasta-termica/',
+ 'reparatii-controllere-ps4-ps5-xbox/', 'mentenanta-console-ps4-ps5-xbox/', 'mentenanta-profesionala-pasta-termica/', 'mentenanta-periodica/',
  'reparatii-laptop-sector-6/', 'reparatii-macbook-sector-6/', 'reparatii-console-gaming-sector-6/',
  'reparatii-boxe-active-amplificatoare/', 'reparatii-monitoare-sector-6/', 'reparatii-tv-sectoare/',
  'termeni-si-conditii/', 'politica-de-confidentialitate/', 'gdpr/',
