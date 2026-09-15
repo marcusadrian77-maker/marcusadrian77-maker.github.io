bbc75489129b3c732957bb82caaa3a57990e5063
marcusadrian77-maker <marcusadrian77@gmail.com>
2026-09-15 19:38:53 +0300

    Add new TV repair related URLs to sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -15,6 +15,7 @@ const statice = ['', 'despre-noi/', 'servicii/', 'preturi/', 'contact/', 'intreb
  'reparatii-tv-hisense/', 'reparatii-tv-led-bucuresti/',
  'reparatii-boxe-portabile-bluetooth/',
  'dungi-pe-ecranul-televizorului/', 'televizor-fara-sunet/', 'televizor-se-restarteaza-singur/', 'reparatii-tv-buzau/',
+ 'televizorul-se-aude-dar-nu-se-vede/', 'televizor-trasnit/', 'inlocuire-leduri-tv/',
  'reparatii-tv-langa-bucuresti/', 'reparatii-tv-ilfov/', 'reparatii-tv-giurgiu/', 'reparatii-tv-dambovita/', 'reparatii-tv-prahova/', 'reparatii-tv-ialomita/', 'reparatii-tv-calarasi/', 'reparatii-tv-teleorman/',
  'reparatii-soundbar/', 'reparatii-prin-curier/', 'cum-ajungeti-la-atelier/',
  'macbook-nu-porneste/', 'macbook-lichid-varsat/', 'inlocuire-baterie-macbook/'];
